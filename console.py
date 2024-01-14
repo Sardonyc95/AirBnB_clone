6c1e1ce2603ca9cb874d75d5f74306a1acc4144c
Naodsh <naodadanech1977@gmail.com>
2024-01-14 12:12:58 -0800

    the console

---
+++ console.py
@@ -0,0 +1,143 @@
+#!/usr/bin/python3
+"""
+Console module for the command interpreter.
+"""
+import cmd
+import json
+import shlex
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+
+class HBNBCommand(cmd.Cmd):
+    """Command interpreter class."""
+    
+    prompt = "(hbnb) "
+    valid_classes = ["BaseModel", "User", "Place", "State", "Amenity", "City", "Review"]
+
+    def do_quit(self, arg):
+        """Quit command to exit the program."""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program."""
+        print()
+        return True
+
+    def emptyline(self):
+        """Do nothing on empty line."""
+        pass
+
+    def do_create(self, arg):
+        """Create a new instance of BaseModel."""
+        args = shlex.split(arg)
+        if not args:
+            print("** class name missing **")
+            return
+        class_name = args[0]
+        if class_name not in self.valid_classes:
+            print("** class doesn't exist **")
+            return
+        new_instance = eval(f"{class_name}()")
+        new_instance.save()
+        print(new_instance.id)
+
+    def do_show(self, arg):
+        """Print the string representation of an instance."""
+        args = shlex.split(arg)
+        if not args:
+            print("** class name missing **")
+            return
+        class_name = args[0]
+        if class_name not in self.valid_classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        object_id = args[1]
+        key = class_name + "." + object_id
+        all_objects = storage.all()
+        if key not in all_objects:
+            print("** no instance found **")
+            return
+        print(all_objects[key])
+
+    def do_destroy(self, arg):
+        """Delete an instance based on the class name and id."""
+        args = shlex.split(arg)
+        if not args:
+            print("** class name missing **")
+            return
+        class_name = args[0]
+        if class_name not in self.valid_classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        object_id = args[1]
+        key = class_name + "." + object_id
+        all_objects = storage.all()
+        if key not in all_objects:
+            print("** no instance found **")
+            return
+        del all_objects[key]
+        storage.save()
+
+    def do_all(self, arg):
+        """Print all string representation of all instances."""
+        args = shlex.split(arg)
+        all_objects = storage.all()
+        if not args:
+            print([str(all_objects[key]) for key in all_objects])
+            return
+        class_name = args[0]
+        if class_name not in self.valid_classes:
+            print("** class doesn't exist **")
+            return
+        print([str(all_objects[key]) for key in all_objects if key.startswith(class_name)])
+
+    def do_update(self, arg):
+        """Update an instance based on the class name and id."""
+        args = shlex.split(arg)
+        if not args:
+            print("** class name missing **")
+            return
+        class_name = args[0]
+        if class_name not in self.valid_classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        object_id = args[1]
+        key = class_name + "." + object_id
+        all_objects = storage.all()
+        if key not in all_objects:
+            print("** no instance found **")
+            return
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return
+        attribute_name = args[2]
+        if len(args) < 4:
+            print("** value missing **")
+            return
+        attribute_value = args[3]
+        instance = all_objects[key]
+        try:
+            attribute_value = eval(attribute_value)
+        except (NameError, SyntaxError):
+            pass
+        setattr(instance, attribute_name, attribute_value)
+        instance.save()
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
+
---
+++ models/amenity.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module that define Amenity class that inherits from BaseModel."""
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """Amenity class that inherits from BaseModel."""
+    name = ""
